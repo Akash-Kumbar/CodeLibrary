dd5012c9ca802a9c0c4e40ca36afc9dc2376899c
Akash-Kumbar <59868922+Akash-Kumbar@users.noreply.github.com>
2022-02-26 23:05:09 +0530

    Add files via upload

---
+++ lonelyInteger.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int lonelyInteger(int arr[], int size);
+
+int main()
+{
+    int size, lone;
+    cout << "Enter size of array : ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter the elements of array :\n";
+    for(int i = 0 ; i < size ; i++)
+    {
+        cin >> arr[i];
+    }
+    lone = lonelyInteger(arr, size);
+    cout << lone << "\n";
+    return 0;
+}
+
+int lonelyInteger(int arr[], int size)
+{
+    int temp = arr[0];
+    int count0 = 0, ret;
+
+    if(temp == 0)
+        count0++;
+    for(int i = 1 ; i < size ; i++)
+    {
+        temp = temp ^ arr[i];
+        if(arr[i] == 0)
+            count0++;
+    }
+    if(temp == 0 && count0 == 1)
+        ret = 0;
+    else
+        ret = temp;
+    return ret;
+}
