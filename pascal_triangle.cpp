bc71353fe9478de5ef5dfdb9b782e0af22b54ecd
Akash-Kumbar <59868922+Akash-Kumbar@users.noreply.github.com>
2022-03-22 12:27:47 +0530

    Add files via upload
    Pascal triangle implementation for leetcode

---
+++ pascal_triangle.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution 
+{
+    public:
+        vector<vector<int>> generate(int numRows)
+        {
+            vector<vector<int>> out;
+            vector<int> temp;
+            for(int i = 0 ; i < numRows ; i++)
+            {
+                if(out.size() == 0)
+                {
+                    temp.push_back(1);
+                    out.push_back(temp);
+                    temp.clear();
+                }
+                else if(out.size() == 1)
+                {
+                    temp.push_back(1);
+                    temp.push_back(1);
+                    out.push_back(temp);
+                    temp.clear();
+                }
+                else
+                {
+                    temp.push_back(1);
+                    for(int j = 0 ; j < out[i-1].size() - 1 ; j++)
+                    {
+                        temp.push_back(out[i-1][j] + out[i-1][j+1]);
+                    }
+                    temp.push_back(1);
+                    out.push_back(temp);
+                    temp.clear();
+                }
+            }
+            return out;
+        }
+};
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> out = sol.generate(25);
+    for(auto vec : out)
+    {
+        for(auto x : vec)
+        {
+            cout << x << " ";
+        }
+        cout << "\n";
+    } 
+    return 0;
+}
