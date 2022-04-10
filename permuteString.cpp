02ff416d04203b2027d45ff05636f2a4b2c5c4b1
Akash-Kumbar <59868922+Akash-Kumbar@users.noreply.github.com>
2022-04-10 09:52:30 +0530

    Add files via upload

---
+++ permuteString.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void permute(string select, string remain)
+{
+    if(remain == ""){
+        cout << select << endl;
+        return;
+    }
+    for(int i=0;remain[i];++i){
+        string wk(remain);
+        permute(select + remain[i], wk.erase(i, 1));
+    }
+}
+
+int main()
+{
+    string anagrama;
+    cout << "input character set >";
+    cin >> anagrama;
+    sort(anagrama.begin(), anagrama.end());
+    permute("", anagrama);
+    return 0;
+}
